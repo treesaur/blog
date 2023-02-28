524e10f49e9d1879ba843aa1a09dd58d6852e214
rly <rallenly@gmail.com>
2023-02-27 20:06:49 -0500

    first commit completed but never used with github

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  root "articles#index"
+  
+  resources :articles do
+    resources :comments
+  end
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
